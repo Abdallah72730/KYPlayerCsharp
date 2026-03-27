c6750308b386f174b00c1012c4cffe86f24bbd4b
Abdallah72730 <ianomeit@gmail.com>
2026-03-27 09:11:25 -0600

    Created ratings view for admin

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using KYPlayer.Models.ViewModels;
 
 namespace KYPlayer.Controllers
 {
