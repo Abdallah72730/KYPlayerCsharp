37037779ef0f874bc50591c496e23247d41cc277
Abdallah72730 <ianomeit@gmail.com>
2026-03-27 05:28:14 -0600

    Added player edit view model for player edit page

---
+++ Models/ViewModels/PlayerEditViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KYPlayer.Models.ViewModels
+{
+    public class PlayerEditViewModel : PlayerCreateViewModel
+    {
+        public int PlayerId { get; set; }
+
+        [Display(Name = "Current PSR")]
+        public float CurrentPSR { get; set; }
+
+        [Display(Name ="Total Ratings")]
+        public int TotalRatingsCount { get; set; }
+
+    }
+}
